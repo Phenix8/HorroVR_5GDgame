4088ece926174d64f4298d5d99e5f6c02dab0eea
Phenix8 <clement.mezelle@free.fr>
2018-05-24 13:03:50 +0200

    Avec sons radio + vent + chuchotement
    Calibrage des paramètres nécessaire

---
+++ Assets/Scripts/1st version prototype/Entite.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entite : MonoBehaviour
+{
+
+    
+    public Transform playerTransform;
+
+    public float angleTolerance = 35.0f;
+
+
+    void Update()
+    {
+        Vector3 posToLookAt = playerTransform.position;
+        posToLookAt.y = transform.position.y;
+        transform.LookAt(posToLookAt);
+
+        Vector3 targetDir = transform.position - playerTransform.position;
+        float angle = Vector3.Angle(targetDir, playerTransform.forward);
+
+        print("Angle calculé : " +angle);
+
+
+        if (angle < angleTolerance)
+        {
+            Vector3 newPosition = playerTransform.position;
+            newPosition -= playerTransform.forward;
+            this.transform.position = newPosition;
+        }
+               
+        
+    }    
+
+}
+
