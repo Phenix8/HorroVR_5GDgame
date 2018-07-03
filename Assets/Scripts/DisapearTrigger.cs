982307c95531d7ec04f43e3460697624c44c2be8
Phenix8 <clement.mezelle@free.fr>
2018-07-03 19:05:05 +0200

    Script pour cacher les éléments portant un certain tag au contact d'un trigger

---
+++ Assets/Scripts/DisapearTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisapearTrigger : MonoBehaviour
+{
+
+    public string tagToEnable;
+
+
+    void Start()
+    {
+
+    }
+
+
+
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (FogManager.triggers.ContainsKey(tagToEnable))
+            FogManager.triggers[tagToEnable] = true;
+    }
+
+
+}
--- Assets/Scripts/FogManager.cs
+++ Assets/Scripts/FogManager.cs
@@ -1,33 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FogManager : MonoBehaviour {
 
     public ColorfulFog fog;
-    public float fogStartDensity = 0.00f;
-    public float fogGoalDensity = 0.02f;
+    private float fogStartDensity = 0.00f;
+    private float fogStrongDensity = 0.02f;
+
+    public int disappearStepsNb = 5;
 
     private bool enableFog = false;
 
+    // Booléen de désactivation des objets par tag
+    public static Dictionary<string, bool> triggers;
+
+
     void Start ()
     {
-        if (fog != null)
-            fog.fogDensity = fogStartDensity;
+        fogStartDensity = fog.fogDensity;
+        fogStrongDensity = fog.fogDensity * 2.0f;
+
+        triggers = new Dictionary<string, bool>();
+        for (int i = 1; i <= disappearStepsNb; i++)
+            triggers.Add("toHideStep" + i, false);
     }
 	
 
 
 	void Update () {
 
-        if (enableFog && fog.fogDensity < fogGoalDensity)
-            fog.fogDensity += 0.001f;
+        foreach (string tag in triggers.Keys)
+        {
+            if (triggers[tag])
+                MakeDisapearObjects(tag);
+        }
 
     }
 
 
-    private void OnTriggerEnter(Collider other)
+    private void MakeDisapearObjects(string tag)
     {
-        enableFog = true;
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
+
+        if (objects.Length == 0)
+        {
+            print("No object found to make disapear.");
+            return;
+        }
+
+        foreach (GameObject obj in objects)
+            obj.SetActive(false);
+
+        triggers[tag] = false;
     }
+
+
 }
